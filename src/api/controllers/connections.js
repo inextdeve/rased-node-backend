fc538da7b5eae8adc34cdf6e2389e1f5bcc82aed
Yassine Farroud <yassinefarroud@protonmail.com>
2024-11-12 07:54:03 +0100

    connection controller

--- src/api/controllers/connections.js
+++ src/api/controllers/connections.js
@@ -5,13 +5,14 @@ export const binTagConnection = async (req, res) => {
   console.log(req.body);
   const { binid, tagid } = req.body;
   try {
-    if (!binid || !tagid) throw new Error("Bad Request");
+    if (binid === undefined || tagid === undefined)
+      throw new Error("Bad Request");
     db = await dbPools.pool.getConnection();
     await db.query("CALL UpdateBinTagRelationship(?, ?)", [tagid, binid]);
-    return res.status(200).end();
+    return res.status(200).send("OK");
   } catch (error) {
     console.log(error);
-    return res.status(400).end("Server Error");
+    return res.status(400).send("Server Error");
   } finally {
     if (db) {
       db.release();
