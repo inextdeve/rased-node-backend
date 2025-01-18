e01d551ec78036097491e32da6bd64aa83be296f
inext.dev <33235992+inextdeve@users.noreply.github.com>
2025-01-18 13:42:46 -0800

    database env

--- src/api/db/config/index.js
+++ src/api/db/config/index.js
@@ -9,7 +9,7 @@ export default Object.freeze({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "rased-track",
+    database: process.env.DB_NAME,
     connectionLimit: 250,
   }),
 });
